0626b53c64e8e7a00ea565a2f7b6afcef23bf16a
Luciano Mammino <lmammino@oryzone.com>
2015-01-13 00:04:57 +0000

    Added support for custom server.js init script

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ By using this installer you will be able to start/stop a prerender instance as s
 sudo service prerender start|stop|restart
 ```
 
-![Install script running example image](http://oi61.tinypic.com/os4v8k.jpg)
 
 ## Install
 
@@ -27,6 +26,8 @@ Then run
 sudo prerender-daemon/install.sh
 ```
 
+![Install script running example image](http://oi61.tinypic.com/os4v8k.jpg)
+
 and it will do all the following **hard work** for you:
 
  - Install prerender as global package
@@ -40,6 +41,13 @@ The installation command support few parameters:
  - `-h` or `--help`: Display the help
  - `-v` or `--verbose`: more verbose output
  - `-u` or `--uninstall`: Act as an unistaller removing prerender-daemon
+ - `-p` or `--node-prefix`: specify a custom node prefix (eg. "/usr")
+ - `-s` or `--server-script`: specify a custom server script that will be used to replace the default launch script (eg. to provide a custom configuration)
+
+
+## Advanced configuration
+
+If you want to provide a custom script to run your prerender server (with custom configuration variables and plugins) you can use a custom `server.js` file (you have an example on [`config/server.js.dist`](/config/server.js.dist)) and pass its path as `--server-script` parameter to the installer script. This file will be copied and substituted to the default `server.js` script in your `node_modules` folder.
 
 
 ## Uninstall
--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 #variables
 VERSION="1.0.0"
 SOURCE=$(dirname ${BASH_SOURCE[0]})
-INITSCRIPT="$SOURCE/prerender.init";
+INITSCRIPT="$SOURCE/prerender.init"
+SERVERSCRIPT=""
+DEFAULT_NODE_PREFIX="/usr"
+NODE_PREFIX=$DEFAULT_NODE_PREFIX
 bold=`tput bold`
 normal=`tput sgr0`
 
@@ -25,6 +28,14 @@ do
 		-v|--verbose)
 			VERBOSE=true
 		;;
+
+		-p=*|--node-prefix=*)
+	    	NODE_PREFIX="${i#*=}"
+	    ;;
+
+	    -s=*|--server-script=*)
+	    	SERVERSCRIPT="${i#*=}"
+	    ;;
 	esac
 done
 
@@ -36,30 +47,18 @@ banner() {
 	echo ""
 }
 
-# display help
-if [ "$SHOW_HELP" = true ]; then
-	banner;
-
-	cat << EOF
- Usage:
-
-	sudo ./install.sh [-h|--help] [-u|--uninstall]
-
- options:
-
-	-h|--help		Display this help
-	-u|--uninstall		Uninstall prerender
-	-v|--verbose		Enable verbose output
-
-EOF
-
-	exit 0;
-fi
-
-#should be run as sudo
-if [ "$UID" -ne 0 ]; then
-	echo "WARNING: it seems that the installer has not be run as root. It may not work as intended."
-fi
+rootcheck() {
+	#should be run as sudo from here
+	if [ "$UID" -ne 0 ]; then
+		echo "${bold}WARNING: it seems that the installer has not be run as root${normal}. It may not work as intended."
+	    read -p "${bold}Do you want to continue? (yn)${normal} [n]" yn
+	    case $yn in
+	        [Yy]* ) ;;
+	        [Nn]* ) exit;;
+	        * ) exit;;
+	    esac
+	fi
+}
 
 #manage verbosity
 exec 3>&1
@@ -69,15 +68,50 @@ if [ "$VERBOSE" = false ]; then
 	exec 2>/dev/null
 fi
 
+# display help
+if [ "$SHOW_HELP" = true ]; then
+	banner 1>&3 2>&4
+
+	cat 1>&3 2>&4 << EOF
+ Usage:
+
+	sudo ./install.sh [-h|--help] [-u|--uninstall]
+
+ Options:
+
+	-h|--help			Display this help
+	-u|--uninstall			Uninstall prerender
+	-v|--verbose			Enable verbose output
+	(-p|--node-prefix)=VALUE	specify a custom node prefix (default "${DEFAULT_NODE_PREFIX}")
+	(-s|--server-script)=VALUE	specify a custom server script that will be used to replace the default launch script (eg. to provide a custom configuration)
+
+ Examples:
+
+ 	Install with default options:
+ 		sudo ./install.sh
+ 	
+ 	Uninstall:
+ 		sudo ./install.sh --uninstall
+
+ 	Install with custom node prefix and using a custom server script:
+ 		sudo ./install.sh --node-prefix=usr/local --server-script=~/temp/my-custom-script.sh
+
+EOF
+	exit 0;
+fi
+
 #uninstall
 if [ "$UNINSTALL" = true ]; then
 	banner 1>&3 2>&4
+	rootcheck 1>&3 2>&4
+
 	echo " ${bold}Uninstalling prerender-daemon${normal}" 1>&3 2>&4
 
 	/etc/init.d/prerender stop
 	echo "  ${bold}✓${normal} Daemon stopped" 1>&3 2>&4
 
 	rm /etc/init.d/prerender
+	update-rc.d prerender remove
 	echo "  ${bold}✓${normal} Init script deleted" 1>&3 2>&4
 
 	rm -rf /var/run/prerender
@@ -100,10 +134,18 @@ fi
 
 #install
 banner 1>&3 2>&4
+rootcheck 1>&3 2>&4
 echo " ${bold}Installing prerender-daemon${normal}" 1>&3 2>&4
 
 npm install --global prerender 1>&3 2>&4
 echo "  ${bold}✓${normal} npm prerender installed" 1>&3 2>&4
+
+if [ -e "$SERVERSCRIPT" ]; then
+	DEFAULTSCRIPT="$NODE_PREFIX/lib/node_modules/prerender/server.js"
+	mv $DEFAULTSCRIPT "$DEFAULTSCRIPT.bak"
+	cp $SERVERSCRIPT $DEFAULTSCRIPT
+	echo "  ${bold}✓${normal} Server script copied to ${DEFAULTSCRIPT}" 1>&3 2>&4
+fi
 
 useradd -r -s /bin/false prerender
 echo "  ${bold}✓${normal} prerender user and user group added" 1>&3 2>&4
